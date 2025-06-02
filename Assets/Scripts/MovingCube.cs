b1ad5032e1a669b17eba98313813c59c6f768af2
CHEEWOO HONG <1974cldn@gmail.com>
2025-06-02 14:59:00 +0900

    [Feat]점프 맵 틀 생성

--- Assets/Scripts/MovingCube.cs
+++ Assets/Scripts/MovingCube.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class MovingCube : MonoBehaviour
 {
-    [SerializeField] private Vector3 moveDirection = Vector3.right;
-    [SerializeField] private float moveDistance = 3f;
-    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private Vector3 moveDirection = Vector3.right; // 이동 방향
+    [SerializeField] private float moveDistance = 3f; // 이동 거리
+    [SerializeField] private float moveSpeed = 2f; // 이동 속도
 
     private Vector3 startPos;
     private Vector3 targetPos;
@@ -20,7 +20,7 @@ public class MovingCube : MonoBehaviour
 
     void Update()
     {
-        Vector3 destination = movingToTarget ? targetPos : startPos;
+        Vector3 destination = movingToTarget ? targetPos : startPos; // 목적지면 시작위치로 시작위치면 목적지로 설정
         transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
 
         // 목적지 도달 시 방향 전환
